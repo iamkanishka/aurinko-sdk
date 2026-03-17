20878ea5e8eda91e8f3f2788cf8b5c2d84a7410d
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2026-03-17 17:21:32 +0530

    index.ts

---
+++ src/resources/index.ts
@@ -0,0 +1,7 @@
+export { EmailResource } from "./email";
+export { CalendarResource } from "./calendar";
+export { ContactsResource } from "./contacts";
+export { TasksResource } from "./tasks";
+export { WebhooksResource } from "./webhooks";
+export { BookingResource, GroupBookingResource } from "./booking";
+export { DirectResource, AuthResource } from "./direct";
